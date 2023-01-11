87bfabb8ea9ac7e645f25addc43ee66a5ced52b1
unknown <erkroch@gmail.com>
2023-01-11 13:01:23 +0100

    header section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
@@ -9,7 +10,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello</h1>
+      {/*HEADER*/}
+      <Header />
+
+      {/*BODY*/}
+
+      {/*FOOTER*/}
     </>
   );
 }
