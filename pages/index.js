0fef17260cb3ecf8f433fd8096f15e893af4fbd3
unknown <erkroch@gmail.com>
2023-01-12 06:34:12 +0100

    create body section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Image from 'next/image';
+import { MicrophoneIcon } from '@heroicons/react/solid';
+import { SearchIcon } from '@heroicons/react/solid';
 
 export default function Home() {
   return (
@@ -14,6 +17,24 @@ export default function Home() {
       <Header />
 
       {/*BODY*/}
+      <form className="flex flex-col items-center mt-40">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png"
+          alt="logo"
+          width="300"
+          height="100"
+          objectFit="cover"
+        />
+        <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 text-gray-500 mr-3" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+          <button className="btn">Google search</button>
+          <button className="btn">I`m Feeling Lucky</button>
+        </div>
+      </form>
 
       {/*FOOTER*/}
     </>
