0a535d433e43fdee7cfaf30efeaa8321be9ea3cb
unknown <erkroch@gmail.com>
2023-01-14 06:07:26 +0100

    complete the result section and install html react parser

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Parser from 'html-react-parser';
 
 export default function SearchResult({ results }) {
   return (
@@ -7,6 +8,24 @@ export default function SearchResult({ results }) {
         About {results.searchInformation.formattedTotalResults} results (
         {results.searchInformation.formattedSearchTime} seconds)
       </p>
+      {results.items.map((result) => (
+        <div key={result.link} className="max-w-xl mb-8">
+          <div className="group">
+            <a className="text-sm truncate" href={result.link}>
+              {result.formattedUrl}
+            </a>
+            <a
+              className="group-hover:underline decoration-blue-800"
+              href={result.link}
+            >
+              <h2 className="truncate text-xl font-medium text-blue-600">
+                {result.title}
+              </h2>
+            </a>
+          </div>
+          <p className="text-gray-700">{Parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>
   );
 }
